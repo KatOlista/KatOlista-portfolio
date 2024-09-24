88f7e679e607b1b2a543149fe3a20a601106443d
KatOlista^^! <olya.katyushina@gmail.com>
2024-09-24 09:23:39 +0200

    delete redundant files, fix eslint config

--- src/components/ProjectItem/ProjectItem.tsx
+++ src/components/ProjectItem/ProjectItem.tsx
@@ -16,7 +16,9 @@ export const ProjectItem: React.FC<Props> = ({ project }) => {
   const showDescriptionHandler = () => {
     const currentItem = document.querySelector(`#item-${project.id}`);
 
-    document.getElementById('Projects')?.classList.add(`${styles.project__index}`);
+    document
+      .getElementById('Projects')
+      ?.classList.add(`${styles.project__index}`);
 
     currentItem?.classList.add(`${styles.project__current}`);
 
@@ -28,7 +30,9 @@ export const ProjectItem: React.FC<Props> = ({ project }) => {
       .querySelector(`#item-${project.id}`)
       ?.classList.remove(`${styles.project__current}`);
 
-      document.getElementById('Projects')?.classList.remove(`${styles.project__index}`);;
+    document
+      .getElementById('Projects')
+      ?.classList.remove(`${styles.project__index}`);
 
     document.body.classList.remove('noscroll');
   };
