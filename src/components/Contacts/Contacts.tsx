83e6b5eadc60863ee8b6e6aa4adfe167748a2787
KatOlista^^! <olya.katyushina@gmail.com>
2024-11-07 10:56:55 +0100

    remove animation appearance from project and contact sections

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,31 +1,9 @@
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
 import { contacts } from '../../utils';
 import { ContactItem, Title } from '..';
 
 import styles from './Contacts.module.scss';
 
 export const Contacts = () => {
-  gsap.registerPlugin(ScrollTrigger);
-
-  useGSAP(() => {
-    gsap.timeline()
-      .from(`.${styles.contacts}`, {
-        scrollTrigger: {
-          trigger: `.${styles.contacts}`,
-          start: '-20% 80%',
-          end: '0%',
-          scrub: 1,
-        },
-        opacity: 0,
-        y: '25%',
-        delay: 1,
-        duration: 1,
-      })
-    });
-
   return (
     <section id="Contacts" className={styles.contacts}>
       <div className="container">
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,31 +1,9 @@
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
 import { projects } from '../../utils';
 import { ProjectItem, Title } from '..';
 
 import styles from './Projects.module.scss';
 
 export const Projects = () => {
-  gsap.registerPlugin(ScrollTrigger);
-
-  useGSAP(() => {
-    gsap.timeline()
-      .from(`.${styles.projects}`, {
-        scrollTrigger: {
-          trigger: `.${styles.projects}`,
-          start: 'top 80%',
-          end: '5%',
-          scrub: 1,
-        },
-        opacity: 0,
-        y: '10%',
-        delay: 1,
-        duration: 1,
-      })
-    });
-
   return (
     <section id="Projects" className={styles.projects}>
       <div className="container">
